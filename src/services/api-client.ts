5fdae2928d175e95f68095ceff806643194b0f9d
Dima Domnich <dima.domnich.mail@gmail.com>
2023-06-01 09:27:18 +0300

    initial commit

---
+++ src/services/api-client.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    key: '83c967e26c5c4b02a1153edd79ccaf2f',
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
